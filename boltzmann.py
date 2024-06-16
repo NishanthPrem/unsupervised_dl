c1aba00404068845c1591bf9b9352434938f4a17
Nishanth Prem <nishanthprem8@gmail.com>
2024-06-16 10:11:53 -0700

    Updated Code.

--- boltzmann.py
+++ boltzmann.py
@@ -3,11 +3,8 @@
 import pandas as pd
 import numpy as np
 import torch
-import torch.nn as nn
 import torch.nn.parallel
 import torch.utils.data
-import torch.optim as optim
-from torch.autograd import Variable
 
 #%% Loading the dataset
 
@@ -32,8 +29,8 @@ test_set = np.array(test_set, dtype='int')
 
 #%% Finding the max numbers in the training and test set
 
-nb_users = max(max(train_set[:,0]), max(test_set[:,0]))
-nb_movies = max(max(train_set[:,1]), max(test_set[:,1]))
+nb_users = int(max(max(train_set[:,0]), max(test_set[:,0])))
+nb_movies = int(max(max(train_set[:,1]), max(test_set[:,1])))
 
 #%% Data conversion function
 
@@ -70,79 +67,64 @@ test_set[test_set >= 3 ] = 1
 #%% Creating the Neural Network
 
 class RBM():
-    def __init__(self, nv, nh):
-        self.W = torch.randn(nv, nh)
-        self.a = torch.randn(1, nh)
-        self.b = torch.randn(1, nv)
-        
-    def sample_h(self, x):
-        wx = torch.mm(x, self.W.t())
-        activation = wx + self.a.expand_as(wx)
-        p_h_given_v = torch.sigmoid(activation)
-        return p_h_given_v, torch.bernoulli(p_h_given_v)
-
-    def sample_v(self, y):
-        wy = torch.mm(y, self.W)
-        activation = wy + self.b.expand_as(wy)
-        p_v_given_h = torch.sigmoid(activation)
-        return p_v_given_h, torch.bernoulli(p_v_given_h)
+  def __init__(self, nv, nh):
+    self.W = torch.randn(nh, nv)
+    self.a = torch.randn(1, nh)
+    self.b = torch.randn(1, nv)
     
-    def train(self, v0, vk, ph0, phk):
-        self.W += torch.mm(v0.t(), ph0) - torch.mm(vk.t(), phk)
-        self.b += torch.sum((v0 - vk), 0)
-        self.a += torch.sum((ph0, phk), 0)
+  def sample_h(self, x):
+    wx = torch.mm(x, self.W.t())
+    activation = wx + self.a.expand_as(wx)
+    p_h_given_v = torch.sigmoid(activation)
+    return p_h_given_v, torch.bernoulli(p_h_given_v)
 
+  def sample_v(self, y):
+    wy = torch.mm(y, self.W)
+    activation = wy + self.b.expand_as(wy)
+    p_v_given_h = torch.sigmoid(activation)
+    return p_v_given_h, torch.bernoulli(p_v_given_h)
+
+  def train(self, v0, vk, ph0, phk):
+    self.W += (torch.mm(v0.t(), ph0) - torch.mm(vk.t(), phk)).t()
+    self.b += torch.sum((v0 - vk), 0)
+    self.a += torch.sum((ph0 - phk), 0)
+    
 nv = len(train_set[0])
 nh = 100
 batch_size = 100
 rbm = RBM(nv, nh)
+
         
 #%% Training the RBM
 
-epochs = 10
-
-for epoch in range(1, epochs + 10):
+nb_epoch = 10
+for epoch in range(1, nb_epoch + 1):
     train_loss = 0
     s = 0.
-    
     for id_user in range(0, nb_users - batch_size, batch_size):
-        vk = train_set[id_user: id_user+batch_size]
-        v0 = train_set[id_user: id_user+batch_size]
-        ph0, _ = rbm.sample_h(vk)
-        
+        vk = train_set[id_user:id_user+batch_size]
+        v0 = train_set[id_user:id_user+batch_size]
+        ph0,_ = rbm.sample_h(v0)
         for k in range(10):
-            _, hk = rbm.sample_h(vk)
-            _, vk = rbm.sample_v(hk)
+            _,hk = rbm.sample_h(vk)
+            _,vk = rbm.sample_v(hk)
             vk[v0<0] = v0[v0<0]
-            
-        phk, _ = rbm.sample_h(vk)
+        phk,_ = rbm.sample_h(vk)
         rbm.train(v0, vk, ph0, phk)
-        train_loss += torch.mean(torch.abs(v0[v0>=0] - vk[v0>=0]))
+        train_loss += np.sqrt(torch.mean((v0[v0>=0] - vk[v0>=0])**2)) # RMSE here
         s += 1.
-    print(f'Epoch: {str(epoch)} loss: {str(train_loss)}')
+    print('epoch: '+str(epoch)+' loss: '+str(train_loss/s))
 
 #%% Testing the RBM
         
 test_loss = 0
 s = 0.
-
-for id_user in range(0, nb_users - batch_size, batch_size):
-    v = train_set[id_user: id_user+1]
-    vt = test_set[id_user: id_user+1]
-    
+for id_user in range(nb_users):
+    v = train_set[id_user:id_user+1]
+    vt = test_set[id_user:id_user+1]
     if len(vt[vt>=0]) > 0:
-        _, h = rbm.sample_h(v)
-        _, v = rbm.sample_v(h)
-        test_loss += torch.mean(torch.abs(vt[vt>=0]-v[vt>=0]))
+        _,h = rbm.sample_h(v)
+        _,v = rbm.sample_v(h)
+        test_loss += np.sqrt(torch.mean((vt[vt>=0] - v[vt>=0])**2)) # RMSE here
         s += 1.
-print(f'Test loss: {str(test_loss)}')    
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+print('test loss: '+str(test_loss/s))
